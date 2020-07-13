34bc5be6955eee04887fa96eec1e15aa4232175b
Stephen J. Eglen <sje30@cam.ac.uk>
2020-07-13 21:56:53 +0100

    add pdfs

---
+++ runone.R
@@ -0,0 +1,38 @@
+
+source('chick_pipp.R')
+
+nreps = 99
+
+type = "Blue"; field = "DN1"
+
+
+stopifnot(is.element(field, fields))
+stopifnot(is.element(type, types))
+
+
+expt.col = "blue"
+
+deltas = seq(from=5, to=10, by=1)
+sigma = seq(from=1, to=10, by=2)
+kappa = seq(from=1, to=6, by=2)
+
+grid = as.matrix(expand.grid(deltas, sigma, kappa))
+
+ngrid = nrow(grid)
+
+
+
+options(mc.cores=60)
+results = mclapply(1:ngrid, run.one)
+
+results_df = do.call(rbind, results)
+
+write.csv(results_df, file.path("res", sprintf("%s_%s.csv", field, type)))
+
+p = read_pts("DN1", "Red")
+
+
+res = try_field("DN2", "Blue", 5, 1, 1)
+res = try_field("DN2", "Blue", 10, 7, 4)
+
+
