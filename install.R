eaa12c83f05f1047ab292e29bc8f7a1f06a392a8
Stephen J. Eglen <sje30@cam.ac.uk>
2020-07-16 22:45:08 +0100

    installation script

---
+++ install.R
@@ -0,0 +1,8 @@
+## run this file to install the packages.
+install.packages(c("readxl", "devtools"))
+
+devtools::install_github("sje30/sjedrp")
+devtools::install_github("sje30/sjevor")
+devtools::install_github("sje30/sjedist")
+devtools::install_github("sje30/sjedmin")
+
